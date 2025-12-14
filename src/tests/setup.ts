c04b7f469ad070a4620152a4c5b278ebbeb253f7
Ravi Raizada <raviraizadagm@gmail.com>
2025-12-14 22:29:17 +0530

    Add type declarations for jest-dom matchers in vitest

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,7 +2,14 @@ import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
+// Extend vitest's expect with jest-dom matchers
 expect.extend(matchers);
+
+// Add type declarations for jest-dom matchers
+declare module 'vitest' {
+    interface Assertion<T = any> extends jest.Matchers<void, T> { }
+    interface AsymmetricMatchersContaining extends jest.Matchers<void, any> { }
+}
 
 afterEach(() => {
     cleanup();
